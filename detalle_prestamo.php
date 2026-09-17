1a73d065316d3bc6859d1d66092c710028cc7b7b
Gonzalwz <147365916+Gonzalwz@users.noreply.github.com>
2026-09-17 16:24:33 -0600

    Actualizar navegación en detalle_prestamo.php

--- detalle_prestamo.php
+++ detalle_prestamo.php
@@ -81,8 +81,13 @@ $detalle_prestamo = $conexion->query("
 
     <nav>
         <a href="index.php">Inicio</a>
+        <a href="editoriales.php">Editoriales</a>
+        <a href="autores.php">Autores</a>
+        <a href="libros.php">Libros</a>
+        <a href="Estudiantes.php">Estudiantes</a>
         <a href="prestamos.php">Prestamos</a>
-        <!-- Aquí se agregan los links a las demás tablas cuando estén listas -->
+        <a href="detalle_prestamo.php">Detalle de los Prestamos</a>
+        <a href="devoluciones.php">Devoluciones</a>
     </nav>
 
     <h1>Prestamos</h1>
