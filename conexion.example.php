4982ea251f643a8f0380f873d3e22f02228b5d44
Alex Gonzalez <agonzalezv36@gmail.com>
2026-09-17 02:52:55 -0600

    Agregar .gitignore y archivo de ejemplo, remover credenciales

--- conexion.example.php
+++ conexion.example.php
@@ -3,17 +3,15 @@
 $host = "127.0.0.1";
 $puerto = 3306;
 $usuario = "root";
-$contrasena = "123456"; // <-- reemplaza con tu contraseña real de MySQL
+$contrasena = "123456";
 $basedatos = "Proyecto_biblioteca";
 
 // Crear la conexión
 $conexion = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
 
-// Verificar que la conexión sí se haya hecho
 if ($conexion->connect_error) {
     die("Error de conexión a la base de datos: " . $conexion->connect_error);
 }
 
-// Para que los acentos (á, é, í, ó, ú, ñ) se guarden y muestren bien
 $conexion->set_charset("utf8mb4");
 ?>
