e94d95c0e6a7dfe3a925d18fc415f95f38d16cc1
Gonzalwz <147365916+Gonzalwz@users.noreply.github.com>
2026-09-17 16:28:48 -0600

    Actualizar navegación en devoluciones.php

--- devoluciones.php
+++ devoluciones.php
@@ -92,8 +92,13 @@ $devoluciones = $conexion->query("
 
     <nav>
         <a href="index.php">Inicio</a>
+        <a href="editoriales.php">Editoriales</a>
+        <a href="autores.php">Autores</a>
+        <a href="libros.php">Libros</a>
+        <a href="Estudiantes.php">Estudiantes</a>
+        <a href="prestamos.php">Prestamos</a>
+        <a href="detalle_prestamo.php">Detalle de los Prestamos</a>
         <a href="devoluciones.php">Devoluciones</a>
-        <!-- Aquí se agregan los links a las demás tablas cuando estén listas -->
     </nav>
 
     <h1>Devoluciones</h1>
